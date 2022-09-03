56a41d3576bcc5cec54d31e327c7b649cdecb9c5
Loresea753 <woratan.2499@mail.kmutt.ac.th>
2022-09-03 14:57:01 +0700

    testv1.1

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -7,11 +7,11 @@ function calculateBMI(weight, height) {
 function getBMIMeaning(weight, height) {
   let srt
   calculateBMI(weight,height)
-  if(result<18.5){
+  if(calculateBMI()<18.5){
     return str = 'Underweight'
-  }else if(result>=18.5&&result<25){
+  }else if(calculateBMI()>=18.5&&result<25){
     return str = 'Noremal weight'
-  }else if(result>=25.0){
+  }else if(calculateBMI()>=25.0){
     return str = 'Overweight'
   }
 }
