f18540f050432f72b7ab4a3e0492ea2540aba519
Loresea753 <woratan.2499@mail.kmutt.ac.th>
2022-09-03 14:52:41 +0700

    testv1

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,18 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  let result = weight/(height*height)
+  return result
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  let srt
+  calculateBMI(weight,height)
+  if(result<18.5){
+    return str = 'Underweight'
+  }else if(result>=18.5&&result<25){
+    return str = 'Noremal weight'
+  }else if(result>=25.0){
+    return str = 'Overweight'
+  }
 }
 module.exports = getBMIMeaning
